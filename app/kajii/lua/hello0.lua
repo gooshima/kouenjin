4a5a88dc06c950e2f110d6a04fc1778f735f5dee
GoOshima <goshima5@gmail.com>
2015-06-14 12:14:13 +0900

    delete unused files

--- app/kajii/lua/hello0.lua
+++
@@ -1,4 +0,0 @@
-print("neon test")
-local file = io.open("PATTERN.DAT", "w+")
-file:write(0)
-file:close()
--- app/kajii/lua/hello4.lua
+++
@@ -1,4 +0,0 @@
-print("neon test")
-local file = io.open("PATTERN.DAT", "w+")
-file:write(4)
-file:close()
